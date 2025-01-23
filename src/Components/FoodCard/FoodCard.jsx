00c2bbe2b718f6edc9bc7f485f07bcb2f5803e9e
reham0000 <amsarkerreham@gmail.com>
2025-01-23 20:30:06 +0600

    added login form

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -12,11 +12,11 @@ const FoodCard = ({item}) => {
       alt="Shoes" />
   </figure>
   <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">${price}</p>
-  <div className="card-body text-center">
-    <h2 className="text-xl font-semibold text-center">{name}</h2>
+  <div className="card-body flex flex-col items-center">
+    <h2 className="card-title">{name}</h2>
     <p>{recipe}</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Add to Cart</button>
+      <button className="btn btn-outline bg-slate-100 border-orange-400 border-0 border-b-4 mt-4">Add to Cart</button>
     </div>
   </div>
 </div>
--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -7,11 +7,14 @@ import useMenu from "../../../hooks/useMenu";
 import FoodCard from "../../../Components/FoodCard/FoodCard";
 import OrderTab from "../OrderTab/OrderTab";
 import { useParams } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const Order = () => {
-  const [tabIndex, setTabIndex] = useState(0);
+  const categories = ["salad", "pizza", "soup", "dessert", "drinks"];
+  const { category } = useParams();
+  const initialIndex = categories.indexOf(category);
+  const [tabIndex, setTabIndex] = useState(initialIndex);
   const [menu] = useMenu();
-  const {category} = useParams();
 
   const desserts = menu.filter((item) => item.category === "dessert");
   const soup = menu.filter((item) => item.category === "soup");
@@ -19,9 +22,11 @@ const Order = () => {
   const pizza = menu.filter((item) => item.category === "pizza");
   const drinks = menu.filter((item) => item.category === "drinks");
 
-
   return (
     <div>
+      <Helmet>
+        <title>Tasty Treat | Order Food</title>
+      </Helmet>
       <Cover img={orderCoverImg} title="Order Food"></Cover>
       <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
         <TabList>
