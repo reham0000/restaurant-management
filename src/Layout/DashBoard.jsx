4f0e517570f7cc071856c5c6d0381aedcc480aa9
reham0000 <amsarkerreham@gmail.com>
2025-02-03 20:39:55 +0600

    secure all route from normal user

--- src/Layout/DashBoard.jsx
+++ src/Layout/DashBoard.jsx
@@ -11,12 +11,13 @@ import { MdOutlineReviews, MdMenuBook } from "react-icons/md";
 import { TbReservedLine } from "react-icons/tb";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../hooks/useCart";
+import useAdmin from "../hooks/useAdmin";
 
 const DashBoard = () => {
   const [cart] = useCart();
 
   // TODO: get isAdmin value from the database.
-  const isAdmin = true;
+  const [isAdmin] = useAdmin();
 
   return (
     <div className="flex">
