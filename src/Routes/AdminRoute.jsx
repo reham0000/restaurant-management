d8f28f1828e1cee15e25d887a645f52a9dc3adb2
reham0000 <amsarkerreham@gmail.com>
2025-02-04 21:12:05 +0600

    make add items form using react hook form

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -2,11 +2,10 @@ import { Navigate, useLocation } from "react-router-dom";
 import useAdmin from "../hooks/useAdmin";
 import useAuth from "../hooks/useAuth";
 
-const AdminRoute = (children) => {
-
-    const [user, loading] = useAuth();
-    const [isAdmin, isAdminLoading] = useAdmin();
-    const location = useLocation();
+const AdminRoute = ({children}) => {
+  const {user, loading} = useAuth();
+  const [isAdmin, isAdminLoading] = useAdmin();
+  const location = useLocation();
 
   if (loading || isAdminLoading) {
     return <progress className="progress w-56"></progress>;
@@ -15,7 +14,7 @@ const AdminRoute = (children) => {
     return children;
   }
 
-  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+  return <Navigate to="/" state={{ from: location }} replace></Navigate>;
 };
 
 export default AdminRoute;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -9,6 +9,8 @@ import DashBoard from "../Layout/DashBoard";
 import Cart from "../Pages/DashBoard/Cart/Cart";
 import PrivetRoute from "./PrivetRoute";
 import AllUsers from "../Pages/DashBoard/AllUsers/AllUsers";
+import AddItems from "../Pages/DashBoard/AddItems/AddItems";
+import AdminRoute from "./AdminRoute";
 
 export const router = createBrowserRouter([
   {
@@ -41,15 +43,20 @@ export const router = createBrowserRouter([
     path:'dashboard',
     element: <PrivetRoute><DashBoard></DashBoard></PrivetRoute>,
     children: [
+    // normal user routes
       {
         path:'cart',
         element:<Cart></Cart>
       },
 
-      // admin routes
+      // admin only routes
+      {
+        path: 'addItems',
+        element: <AdminRoute><AddItems></AddItems></AdminRoute>
+      },
       {
         path: 'users',
-        element: <AllUsers></AllUsers>
+        element: <AdminRoute><AllUsers></AllUsers></AdminRoute>
       }
     ]
   }
